b733838c8294dc3cd864bc2451af83d2342dd7d5
fdeygaushij <esjcalejandro97@hotmail.com>
2024-05-09 14:16:08 +0700

    demo commit

--- index.js
+++ index.js
@@ -1,18 +1,11 @@
-function permute(nums) {
-  const result = [];
-  backtrack([]);
-  return result;
-  function backtrack(permutation) {
-    if (permutation.length === nums.length) {
-      result.push([...permutation]);
-      return;
-    }
-    for (const num of nums) {
-      if (!permutation.includes(num)) {
-        permutation.push(num);
-        backtrack(permutation);
-        permutation.pop();
-      }
-    }
+function findMaxConsecutiveOnes(nums) {
+  let maxCount = 0;
+  let count = 0;
+  for (const num of nums) {
+    if (num === 1) {
+      count++;
+      maxCount = Math.max(maxCount, count);
+    } else count = 0;
   }
+  return maxCount;
 }
