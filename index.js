84e3d8c715e57b00a885fd77a61e676f09af5d63
fdeygaushij <esjcalejandro97@hotmail.com>
2024-05-10 19:17:57 +0700

    update aptos project commit

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
-function findMaxConsecutiveOnes(nums) {
-  let maxCount = 0;
-  let count = 0;
-  for (const num of nums) {
-    if (num === 1) {
-      count++;
-      maxCount = Math.max(maxCount, count);
-    } else count = 0;
+function permuteUnique(nums) {
+  const result = [];
+  nums.sort((a, b) => a - b);
+  backtrack([], new Array(nums.length).fill(false));
+  return result;
+  function backtrack(current, used) {
+    if (current.length === nums.length) {
+      result.push([...current]);
+      return;
+    }
+    for (let i = 0; i < nums.length; i++) {
+      if (used[i] || (i > 0 && nums[i] === nums[i - 1] && !used[i - 1]))
+        continue;
+      current.push(nums[i]);
+      used[i] = true;
+      backtrack(current, used);
+      current.pop();
+      used[i] = false;
+    }
   }
-  return maxCount;
 }
